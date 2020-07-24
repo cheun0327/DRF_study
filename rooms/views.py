3ea1f77e588bbffb92e06c14b20bd493f49743ee
cheun0327 <cheun0327@gmail.com>
2020-07-24 16:30:22 +0900

    Django Pagination done

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
-from . import models
+from django.shortcuts import render, redirect
 from math import ceil
 from datetime import datetime
 from django.http import HttpResponse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
+from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    paginator = Paginator(room_list, 10, orphans=5)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
     # page = request.GET.get("page", 1)
     # page = int(page or 1)
     # page_size = 10
