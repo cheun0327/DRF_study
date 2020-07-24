89b09d97d031fdecad1436755016bc31351317ee
cheun0327 <cheun0327@gmail.com>
2020-07-24 16:07:33 +0900

    Django Pagination

--- rooms/views.py
+++ rooms/views.py
@@ -3,24 +3,30 @@ from . import models
 from math import ceil
 from datetime import datetime
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 
 def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/home.html", {"rooms": rooms})
+    # page = request.GET.get("page", 1)
+    # page = int(page or 1)
+    # page_size = 10
+    # limit = page_size * page
+    # offset = limit - page_size
+    # all_rooms = models.Room.objects.all()[offset:limit]
+    # page_count = ceil(models.Room.objects.count() / page_size)
+    # return render(
+    #     request,
+    #     "rooms/home.html",
+    #     context={
+    #         "rooms": all_rooms,
+    #         "page": page,
+    #         "page_count": page_count,
+    #         "page_range": range(1, page_count),
+    #     },
+    # )
     # return HttpResponse(content="<h1>hello</h1>")
